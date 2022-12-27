a3c88319bacf563e3e7132c3810238f312c9fcb4
alimtegar <alimtegar4@gmail.com>
2022-12-27 22:29:33 +0700

    Update export excel feature

--- src/App.tsx
+++ src/App.tsx
@@ -95,9 +95,9 @@ const App = () => {
 	*/
 
 	return (
-		<div className="flex justify-center bg-gray-100 h-screen">
-			<form className="bg-white w-full sm:w-5/12 p-4 shadow">
-				<div className="flex flex-col h-full">
+		<div className="flex justify-center bg-gray-100 min-h-screen">
+			<form className="bg-white w-full lg:w-5/12 p-4 shadow">
+				<div className="flex flex-col min-h-full">
 					<div className="form-control w-full mb-2">
 						<label className="label">
 							<span className="label-text font-medium text-xs">Input Max. Undian</span>
@@ -123,7 +123,7 @@ const App = () => {
 										type="text"
 										name="title"
 										placeholder="Nama Item"
-										className="input input-bordered w-full text-xs col-span-3"
+										className="input input-bordered w-full text-xs col-span-8 sm:col-span-3"
 										value={item.title}
 										onChange={(e) => handleItemChange(item.id, e)}
 									/>
@@ -132,7 +132,7 @@ const App = () => {
 										name="n"
 										placeholder="Jumlah"
 										min="1"
-										className="input input-bordered w-full text-xs col-span-2"
+										className="input input-bordered w-full text-xs col-span-3 sm:col-span-2"
 										value={item.n ? +item.n : ''}
 										onChange={(e) => handleItemChange(item.id, e)}
 									/>
@@ -141,19 +141,19 @@ const App = () => {
 										name="frame"
 										placeholder="Frame"
 										min="1"
-										className="input input-bordered w-full text-xs col-span-2"
+										className="input input-bordered w-full text-xs col-span-3 sm:col-span-2"
 										value={item.frame ? +item.frame : ''}
 										onChange={(e) => handleItemChange(item.id, e)}
 									/>
 
 									{items.length > 1 ? (
-										<button type="button" onClick={() => removeItem(item.id)} className="btn btn-outline btn-error">
+										<button type="button" onClick={() => removeItem(item.id)} className="btn btn-outline btn-error col-span-2 sm:col-span-1">
 											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 												<path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
 											</svg>
 										</button>
 									) : (
-										<button type="button" className="btn btn-outline btn-error" disabled>
+										<button type="button" className="btn btn-outline btn-error col-span-2 sm:col-span-1" disabled>
 											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
 												<path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
 											</svg>
--- src/Results.tsx
+++ src/Results.tsx
@@ -14,25 +14,42 @@ const Results = () => {
     const [activeTabI, setActiveTabI] = useState(0);
     const [isFlipped, setIsFlipped] = useState(false);
 
-    // function resultsTo2DArray(results: Result[]) {
-    //     // Get the maximum number of lottery numbers
-    //     const maxLength = Math.max(...results.map(result => result.lotteryNumbers.length));
+    function resultsTo2DArray(results: Result[]) {
+        let _2dArray: string[][] = [];
 
-    //     // Initialize the 2D array with empty strings
-    //     const arr = new Array(maxLength + 1).fill('').map(() => new Array(results.length).fill(''));
+        // Sort the numbered items by their frame values
+        results.forEach(result => {
+            result.numberedItems.sort((a, b) => a.frame.localeCompare(b.frame));
+        });
 
-    //     // Set the titles in the first row
-    //     arr[0] = results.map(result => result.item.title);
+        // Initialize the first row of the 2D array with the titles of the numbered items
+        _2dArray[0] = results.reduce((acc, result) => {
+            return acc.concat(result.numberedItems.map(item => item.title));
+        }, [] as string[]);
 
-    //     // Set the lottery numbers in the subsequent rows
-    //     results.forEach((result, colIndex) => {
-    //         result.lotteryNumbers.forEach((num, rowIndex) => {
-    //             arr[rowIndex + 1][colIndex] = num;
-    //         });
-    //     });
+        // Find the maximum value of n across all numbered items
+        const maxN = Math.max(...results.reduce((acc, result) => {
+            return acc.concat(result.numberedItems.map(item => item.n ? item.n : 0));
+        }, [] as number[]));
 
-    //     return arr;
-    // }
+        // Initialize the rest of the rows of the 2D array with empty strings
+        for (let i = 1; i <= maxN; i++) {
+            _2dArray[i] = Array(_2dArray[0].length).fill('');
+        }
+
+        // Fill the 2D array with the lottery numbers
+        let currentColumn = 0;
+        results.forEach(result => {
+            result.numberedItems.forEach(item => {
+                item.lotteryNumbers.forEach((number, numberIndex) => {
+                    _2dArray[numberIndex + 1][currentColumn] = number.toString();
+                });
+                currentColumn++;
+            });
+        });
+
+        return _2dArray;
+    }
 
     const exportToExcel = (data: (string | number)[][]) => {
         const ws = XLSX.utils.aoa_to_sheet(data);
@@ -41,19 +58,17 @@ const Results = () => {
         XLSX.writeFile(wb, 'Undian.xlsx');
     }
 
-    // useEffect(() => drawLotteryNumbers(range, items), []);
-
     return (
-        <div className="bg-gray-100 w-full h-screen">
-            <div className="bg-white flex justify-between p-4 mb-4 shadow">
-                <button className="btn btn-primary" onClick={() => setIsFlipped(true)}>
+        <div className="bg-gray-100 w-full min-h-screen">
+            <div className="bg-white flex flex-col sm:flex-row justify-between p-4 mb-4 shadow">
+                <button type="button" className="btn btn-primary mb-2 sm:mb-0" onClick={() => setIsFlipped(true)}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2" style={{ marginTop: -1 }}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                     </svg>
 
                     GENERATE
                 </button>
-                <a className="btn btn-success">
+                <a className="btn btn-success" onClick={() => exportToExcel(resultsTo2DArray(results))}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2" style={{ marginTop: -1 }}>
                         <path strokeLinecap="round" strokeLinejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5" />
                     </svg>
@@ -62,10 +77,10 @@ const Results = () => {
             </div>
 
             {/* <div className="flex flex-col h-full"> */}
-            <div className="tabs mb-4 px-4">
+            <div className="tabs flex-col sm:flex-row mb-4 px-4">
                 {results?.length && results.map((result, i) => (
                     <a
-                        className={'tab tab-bordered flex-grow h-12' + (activeTabI === i ? ' tab-active' : '')}
+                        className={'tab tab-bordered flex-grow w-full sm:w-auto h-12' + (activeTabI === i ? ' tab-active' : '')}
                         onClick={() => { setActiveTabI(i); setIsFlipped(false) }}
                         key={result.frame}
                     >
